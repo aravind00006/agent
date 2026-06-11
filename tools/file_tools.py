d886c812de893f4ec9a47da3647caaa0dcfe931f
aravind <aravindar433@gmail.com>
2026-06-11 10:23:00 +0530

    Add constants for skipped directories and token limit configuration

--- tools/file_tools.py
+++ tools/file_tools.py
@@ -14,3 +14,11 @@ from langchain_core.tools import tool
 from core.logger import get_logger
 
 _log = get_logger("tools")
+
+_SKIP_DIRS = {
+    "node_modules", ".git", "__pycache__", "dist", "build",
+    ".tox", ".venv", "venv", "env", ".mypy_cache", ".pytest_cache",
+}
+
+_MAX_TOKENS      = 4_000
+_CHARS_PER_TOKEN = 4
